cd908dcbc407d92a95590ba5b8a5626e58aa3e84
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-15 20:12:07 +0100

    Update activity_model.dart
    Making update with the constructor

--- lib/models/activity_model.dart
+++ lib/models/activity_model.dart
@@ -1,8 +1,19 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class Activity {
-  String imageUrl;
+  late String imageUrl;
   String name;
   String type;
   List<String> startTimes;
   int rating;
   int price;
+
+  // the constructor.
+  Activity({
+    required this.imageUrl,
+    required this.name,
+    required this.type,
+    required this.startTimes,
+    required this.rating,
+    required this.price,
+  });
 }
