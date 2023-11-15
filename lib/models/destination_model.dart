23cdedce7fe6c27e58a0047cb88762a6b598dc82
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-15 20:29:30 +0100

    Update destination_model.dart

--- lib/models/destination_model.dart
+++ lib/models/destination_model.dart
@@ -40,4 +40,35 @@ List<Activity> activities = [
       price: 125),
 ];
 
-List<Destination> destinations = [];
+List<Destination> destinations = [
+  Destination(
+      imageUrl: 'assets/images/venice.jpg',
+      city: 'Venice',
+      country: 'Italy',
+      description: 'Visit Venice for an amazing and unforgettable adventure',
+      activities: activities),
+  Destination(
+      imageUrl: 'assets/images/paris.jpg',
+      city: 'Paris',
+      country: 'France',
+      description: 'Visit Paris for an amazing and unforgettable adventure',
+      activities: activities),
+  Destination(
+      imageUrl: 'assets/images/newdelhi.jpg',
+      city: 'New Delhi',
+      country: 'India',
+      description: 'Visit New Delhi for an amazing and unforgettable adventure',
+      activities: activities),
+  Destination(
+      imageUrl: 'assets/images/saopaulo.jpg',
+      city: 'Sao Paulo',
+      country: 'Brazil',
+      description: 'Visit Sao Paulo for an amazing and unforgettable adventure',
+      activities: activities),
+  Destination(
+      imageUrl: 'assets/images/newyork.jpg',
+      city: 'New York ',
+      country: 'United States',
+      description: 'Visit New York for an amazing and unforgettable adventure',
+      activities: activities),
+];
