91abbb247d213f0b70dc36f7604d0052e8694439
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-15 20:35:42 +0100

    Update hotel_model.dart

--- lib/models/hotel_model.dart
+++ lib/models/hotel_model.dart
@@ -14,4 +14,20 @@ class Hotel {
   });
 }
 
-final List<Hotel> hotels = [];
+final List<Hotel> hotels = [
+  Hotel(
+      imageUrl: 'assets/images/hotel0.jpg',
+      name: 'Hotel 0',
+      address: '404 Great Street',
+      price: 175),
+  Hotel(
+      imageUrl: 'assets/images/hotel1.jpg',
+      name: 'Hotel 1',
+      address: '404 Great Street',
+      price: 300),
+  Hotel(
+      imageUrl: 'assets/images/hotel.j2pg',
+      name: 'Hotel 2',
+      address: '404 Great Street',
+      price: 240),
+];
