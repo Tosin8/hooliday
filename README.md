70b37c064b93ffb767fda5a5ca2a303e0ceb653f
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-15 19:43:10 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# hooliday
+
+Project Name: 'Hooliday'
+
+Description: 'This is a travel mobile app made with google flutter. 
+
+Screenshots:
---
+++ android/app/src/main/kotlin/com/example/hooliday/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hooliday
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
