a9a2fbb299df6fd3b6bc3b11c5ec110f8e206136
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-15 20:05:12 +0100

    Updating Constants
    Project Colors

--- lib/extensions/constants.dart
+++ lib/extensions/constants.dart
@@ -1,1 +1,6 @@
+// useMaterial3: true,
+import 'package:flutter/material.dart';
 
+var primaryColor = Color(0xFF3Ebace);
+var hintColor = Color(0xffd8ecf1);
+var scaffoldBackgroundColor = Color(0xfff3f5f7);
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'screens/home_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,15 +13,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Hooliday',
       theme: ThemeData(
         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         // useMaterial3: true,
         primaryColor: Color(0xFF3Ebace),
-        accentColor: Color(0xffd8ecf1),
+        hintColor: Color(0xffd8ecf1),
         scaffoldBackgroundColor: Color(0xfff3f5f7),
       ),
       home: HomeScreen(),
     );
   }
 }
+
+// accents color is now changed to hintcolor. 
