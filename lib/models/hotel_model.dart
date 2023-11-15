9207d631160cbf94cba76464c14f7c7690865f1d
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-15 20:33:27 +0100

    Update hotel_model.dart

--- lib/models/hotel_model.dart
+++ lib/models/hotel_model.dart
@@ -1,6 +1,17 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 class Hotel {
   String imageUrl;
   String name;
   String address;
   int price;
+
+  // creatig the constructor.
+  Hotel({
+    required this.imageUrl,
+    required this.name,
+    required this.address,
+    required this.price,
+  });
 }
+
+final List<Hotel> hotels = [];
