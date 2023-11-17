779e52fc036d409b3b07912d4abf18aa700ebc9b
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-17 21:01:19 +0100

    Update destination_screen.dart
    Refactoring the Navs Button at the upper level.

--- lib/extensions/destination_screen.dart
+++ lib/extensions/destination_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../models/destination_model.dart';
 
@@ -38,10 +39,24 @@ class _DestinationScreenState extends State<DestinationScreen> {
               padding:
                   const EdgeInsets.symmetric(horizontal: 10.0, vertical: 40.0),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   IconButton(
                       onPressed: () => Navigator.pop(context),
-                      icon: Icon(Icons.arrow_back_ios, color: Colors.white))
+                      icon: Icon(Icons.arrow_back_ios, color: Colors.white)),
+                  SizedBox(width: 30),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () => Navigator.pop(context),
+                        icon: Icon(Icons.search, size: 30, color: Colors.white),
+                      ),
+                      IconButton(
+                          onPressed: () => Navigator.pop(context),
+                          icon: Icon(Icons.sort, size: 30),
+                          color: Colors.white),
+                    ],
+                  )
                 ],
               ),
             )
