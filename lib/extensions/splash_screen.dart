11cb03dd74bc5361b66d103decffc96d479ccb15
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-18 00:13:28 +0100

    Updating pubspec with Asset Image

--- lib/extensions/splash_screen.dart
+++ lib/extensions/splash_screen.dart
@@ -5,6 +5,10 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(image: 'image')
+      ),
+    ));
   }
 }
