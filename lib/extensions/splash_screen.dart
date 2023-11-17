b30a7decb7dce4cfcd192333cd237c056300b5bd
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-18 00:22:44 +0100

    Update splash_screen.dart

--- lib/extensions/splash_screen.dart
+++ lib/extensions/splash_screen.dart
@@ -5,9 +5,18 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(image: 'image')
+    return Scaffold(
+        body: Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/splash_screen.jpg'), fit: BoxFit.cover),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Color(0xff252222), Colors.white.withOpacity(0)],
+                begin: Alignment(0.00, -1.00),
+                end: Alignment(0, 1))),
       ),
     ));
   }
