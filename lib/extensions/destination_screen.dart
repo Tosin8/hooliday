e22f55d7efef17f7e40e98a9714d6f788b2c0dab
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-17 20:26:28 +0100

    Update destination_screen.dart

---
+++ lib/extensions/destination_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DestinationScreen extends StatefulWidget {
+  const DestinationScreen({super.key});
+
+  @override
+  State<DestinationScreen> createState() => _DestinationScreenState();
+}
+
+class _DestinationScreenState extends State<DestinationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
