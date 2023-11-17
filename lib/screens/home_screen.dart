1c8da08457dd6df71cf998cd9b09c2181561ca2e
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-17 11:21:16 +0100

    Create Destination Carousel

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -74,33 +74,7 @@ class _HomeScreenState extends State<HomeScreen> {
           // ], // prefer to use the map feature in case of including another icon inorder to prevent manual insertion.
         ),
         const SizedBox(height: 20),
-        Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Top Destinations',
-                    style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                        letterSpacing: 1.5),
-                  ),
-                  Text(
-                    'See All',
-                    style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w600,
-                        letterSpacing: 1.0),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        )
+        
       ],
     )));
   }
