86058998db2f4ca61be4516778ef8b198e2dbb68
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-22 19:45:48 +0100

    Update profile.dart
    done with header

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -6,52 +6,66 @@ class UserProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: [
-        Container(
-            width: double.infinity,
-            height: 300,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/images/profile_bkg1.jpg'),
-                    fit: BoxFit.cover)),
-            child: Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                Colors.black.withOpacity(0.6),
-                Colors.black.withOpacity(0.3),
-              ], begin: Alignment.bottomRight)),
-              child: Column(
-                children: [
-                  SizedBox(height: 30),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.favorite_border_outlined,
-                            color: Colors.white,
-                          )),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.shopping_bag,
-                            color: Colors.white,
-                          ))
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Container(
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: DecorationImage(
-                            image: AssetImage('assets/profile.jpg'))),
-                  )
-                ],
-              ),
-            ))
-      ],
+        body: SafeArea(
+      child: Stack(
+        children: [
+          Container(
+              width: double.infinity,
+              height: 300,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/profile_bkg1.jpg'),
+                      fit: BoxFit.cover)),
+              child: Container(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                  Colors.black.withOpacity(0.6),
+                  Colors.black.withOpacity(0.3),
+                ], begin: Alignment.bottomRight)),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.favorite_border_outlined,
+                              color: Colors.white,
+                            )),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.shopping_bag,
+                              color: Colors.white,
+                            ))
+                      ],
+                    ),
+                    const CircleAvatar(
+                      radius: 50,
+                      backgroundImage: AssetImage('assets/profile.jpg'),
+                    ),
+                    const Text(
+                      'Miranda West',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Work hard in silence, let your\n success be the noise,',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              ))
+        ],
+      ),
     ));
   }
 }
