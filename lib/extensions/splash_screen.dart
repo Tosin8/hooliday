1c46a30d089a9ece18c1e1226c0f7cc67d1e6629
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-23 19:26:00 +0100

    Update splash_screen.dart

--- lib/extensions/splash_screen.dart
+++ lib/extensions/splash_screen.dart
@@ -8,58 +8,31 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      width: MediaQuery.sizeOf(context).width,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage('assets/splash_screen2.jpg'), fit: BoxFit.cover),
-      ),
-      child: Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-            colors: [
-              Colors.black.withOpacity(0.6),
-              Colors.black.withOpacity(0.1),
-            ],
-            begin: Alignment.bottomRight,
-          )),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(vertical: 120),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  'Hooliday',
-                  style: TextStyle(
-                      fontSize: 48,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w400,
-                      fontFamily: 'Fleur De Leah'),
-                ),
-                SizedBox(height: 50),
-                Text(
-                  'Explore Your World',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w700,
-                      height: 0,
-                      letterSpacing: 1.2),
-                ),
-                Text(
-                  'Everyday is an Hooliday',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w300,
-                      height: 0),
-                ),
-                SizedBox(height: 20),
-                SplashBtn()
-              ],
+            width: MediaQuery.sizeOf(context).width,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/splash_screen2.jpg'),
+                  fit: BoxFit.cover),
             ),
-          )),
-    ));
+            child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                  colors: [
+                    Colors.black.withOpacity(0.6),
+                    Colors.black.withOpacity(0.1),
+                  ],
+                  begin: Alignment.bottomRight,
+                )),
+                child: Column(
+                  children: [
+                    Text(
+                      'Hooliday',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ))));
   }
 }
