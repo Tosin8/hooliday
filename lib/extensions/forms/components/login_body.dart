8befff8896788a649a574a9b8e115d59ac832eac
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-25 00:12:33 +0100

    Refactoring

--- lib/extensions/forms/components/login_body.dart
+++ lib/extensions/forms/components/login_body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../../constant.dart';
+import '../forgotpass.dart';
+
 class LoginBody extends StatefulWidget {
   const LoginBody({super.key});
 
@@ -27,8 +30,8 @@ class _LoginBodyState extends State<LoginBody> {
           style: TextStyle(
               color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
         ),
-        SizedBox(height: 10),
-        formBody(),
+        const SizedBox(height: 10),
+        const formBody(),
       ],
     );
   }
@@ -46,8 +49,157 @@ class _formBodyState extends State<formBody> {
   late String email;
   late String password;
   bool remember = false;
+
+  final List<String> errors = [];
   @override
   Widget build(BuildContext context) {
-    return Form(child: Padding(padding: EdgeInsets.symmetric(horizontal: 10)));
+    return Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              TextFormField(
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                onSaved: (newValue) => email = newValue!,
+                onChanged: (value) {
+                  if (value.isNotEmpty && errors.contains(kEmailNullError)) {
+                    setState(() {
+                      errors.remove(kEmailNullError);
+                    });
+                  } else if (!emailValidatorRegExp.hasMatch(value) &&
+                      errors.contains(kInvalidEmailError)) {
+                    setState(() {
+                      errors.remove(kInvalidEmailError);
+                    });
+                  }
+                  ;
+                  return null;
+                },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    errors.add(kEmailNullError);
+                    return '';
+                  } else if (!emailValidatorRegExp.hasMatch(value)) {
+                    errors.add(kInvalidEmailError);
+                    return '';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Enter your email',
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 20),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(28),
+                      borderSide: const BorderSide(color: kTextColor),
+                      gapPadding: 10,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(28),
+                      borderSide: const BorderSide(color: kTextColor),
+                      gapPadding: 10,
+                    ),
+                    suffixIcon: const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 20, 20, 20),
+                      child: Icon(Icons.mail),
+                    )),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                onSaved: (newValue) => password = newValue!,
+                onChanged: (value) {
+                  if (value.isNotEmpty && errors.contains(kPassNullError)) {
+                    setState(() {
+                      errors.remove(kPassNullError);
+                    });
+                  } else if (value.length >= 8 &&
+                      errors.contains(kShortPassError)) {
+                    setState(() {
+                      errors.remove(kShortPassError);
+                    });
+                  }
+                  return null;
+                },
+                validator: (value) {
+                  if (value!.isEmpty && !errors.contains(kPassNullError)) {
+                    setState(() {
+                      errors.add(kPassNullError);
+                    });
+                  } else if (value.length < 8 &&
+                      !errors.contains(kShortPassError)) {
+                    setState(() {
+                      errors.add(kShortPassError);
+                    });
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.visiblePassword,
+                textInputAction: TextInputAction.done,
+
+                obscureText: true, // turning it into a password key.
+
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter your password',
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: const BorderSide(color: kTextColor),
+                    gapPadding: 10,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: const BorderSide(color: kTextColor),
+                    gapPadding: 10,
+                  ),
+                  suffixIcon: const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 20, 20, 20
+                        // getProportionateScreenWidth(20),
+                        // getProportionateScreenWidth(20),
+                        // getProportionateScreenWidth(20),
+                        ),
+                    child: Icon(Icons.lock
+
+                        // height: getProportionateScreenWidth(18),
+                        ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Checkbox(
+                      value: remember,
+                      activeColor: kPrimaryColor,
+                      onChanged: (value) {
+                        setState(() {
+                          remember = value!;
+                        });
+                      }),
+                  const Text('Remember Me'),
+                  const Spacer(),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => ForgotPwd()));
+                    },
+                    child: const Text(
+                      'Forgot Password',
+                      style: TextStyle(decoration: TextDecoration.underline),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
