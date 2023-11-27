2333a8bde7b37b5cda3b3a00a65534784362dc5d
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-27 16:08:54 +0100

    Update calendar_popup.dart

--- lib/extensions/forms/user_explore/calendar_popup.dart
+++ lib/extensions/forms/user_explore/calendar_popup.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class CalendarPopupView extends StatefulWidget {
+  const CalendarPopupView({super.key});
+
+  @override
+  State<CalendarPopupView> createState() => _CalendarPopupViewState();
+}
+
+class _CalendarPopupViewState extends State<CalendarPopupView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
