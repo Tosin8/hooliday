b6a35cc2cd82414d4c6caafdd8491d79a2872c31
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-27 16:32:10 +0100

    Update calendar_popup.dart

--- lib/extensions/forms/user_explore/calendar_popup.dart
+++ lib/extensions/forms/user_explore/calendar_popup.dart
@@ -2,15 +2,22 @@ import 'package:flutter/material.dart';
 
 class CalendarPopupView extends StatefulWidget {
   const CalendarPopupView(
-      {this.initialStartDate,
-      this.initialEndDate,
-      this.onApplyClick,
-      this.onCancelClick,
+      {required this.initialStartDate,
+      required this.initialEndDate,
+      required this.onApplyClick,
+      required this.onCancelClick,
       this.minimumDate,
       this.maximumDate,
       this.barrierDismissible = true,
       super.key});
 
+  final DateTime? minimumDate;
+  final DateTime? maximumDate;
+  final bool barrierDismissible;
+  final DateTime initialStartDate;
+  final DateTime initialEndDate;
+  final Function(DateTime, DateTime) onApplyClick;
+  final Function onCancelClick;
   @override
   State<CalendarPopupView> createState() => _CalendarPopupViewState();
 }
