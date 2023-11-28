c96a20a2ba75a0e2267d86030ccfb024adc3f5d2
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-28 13:40:55 +0100

    Update constants.dart

--- lib/extensions/constants.dart
+++ lib/extensions/constants.dart
@@ -46,6 +46,18 @@ class HotelAppTheme {
     final ColorScheme colorScheme = const ColorScheme.light().copyWith(
       primary: primaryColor,
       secondary: secondaryColor,
-    ); 
+    );
+    final ThemeData base = ThemeData.light(); 
+    return base.copyWith(
+      primaryColor: primaryColor, 
+      indicatorColor: Colors.white, 
+      splashColor: Colors.white24, 
+      splashFactory: InkRipple.splashFactory, 
+      canvasColor: Colors.white,  
+      scaffoldBackgroundColor: Color(0xfff6f6f6), 
+      buttonTheme: ButtonThemeData(colorScheme: colorScheme, textTheme: ButtonTextTheme.primary), 
+      textTheme: _buildTextTheme(base.textTheme), 
+      primaryTextTheme: 
+    )
   }
 }
