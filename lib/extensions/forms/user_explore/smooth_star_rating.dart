9cd753aefc5695778d05b660fa9bcb3cdbf0ae2d
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-29 18:01:45 +0100

    Update smooth_star_rating.dart

--- lib/extensions/forms/user_explore/smooth_star_rating.dart
+++ lib/extensions/forms/user_explore/smooth_star_rating.dart
@@ -41,4 +41,39 @@ class SmoothStarRating extends StatelessWidget {
       ),
     );
   }
+
+  buildStar(BuildContext context, int index) {
+    Icon icon;
+    if (index >= rating) {
+      icon = Icon(
+        defaultIconData,
+        color: borderColor ?? Theme.of(context).primaryColor,
+        size: size,
+      );
+    } else if (index > rating - (allowHalfRating ? 0.5 : 1.0)) {
+      icon = Icon(halfFilledIconData,
+          color: color ?? Theme.of(context).primaryColor, size: size);
+    } else {
+      icon = Icon(filledIconData,
+          color: color ?? Theme.of(context).primaryColor, size: size);
+    }
+
+    return GestureDetector(
+      onTap: () => onRatingChanged(index + 1.0),
+      onHorizontalDragUpdate: (DragUpdateDetails dragDetails) {
+        final RenderBox? box = context.findRenderObject() as RenderBox?;
+        final Offset? pos = box?.globalToLocal(dragDetails.globalPosition);
+        final double i = pos?.dx ?? 0 / size;
+        double rating = allowHalfRating ? i : i.round().toDouble();
+        if (rating > starCount) {
+          rating = starCount.toDouble();
+        }
+        if (rating < 0) {
+          rating = 0.0;
+        }
+        onRatingChanged(rating);
+      },
+      child: icon,
+    );
+  }
 }
