9611de14a0a2ffd230881e28b9a1436359d28aee
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-29 17:49:28 +0100

    Update smooth_star_rating.dart

--- lib/extensions/forms/user_explore/smooth_star_rating.dart
+++ lib/extensions/forms/user_explore/smooth_star_rating.dart
@@ -5,6 +5,19 @@ typedef RatingChangeCallback = void Function(double rating);
 class SmoothStarRating extends StatelessWidget {
   const SmoothStarRating({super.key});
 
+  final int starCount;
+  final double rating;
+  final RatingChangeCallback onRatingChanged;
+  final Color? color;
+
+  final Color? borderColor;
+  final double size;
+  final bool allowHalfRating;
+  final IconData filledIconData;
+  final IconData halfFilledIconData;
+  final IconData defaultIconData;
+  final double spacing;
+
   @override
   Widget build(BuildContext context) {
     return Container();
