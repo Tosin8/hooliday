37e3f95a728927d8110f057078aaf804e8c4f910
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-29 16:49:46 +0100

    Update slider_view.dart

--- lib/extensions/forms/user_explore/slider_view.dart
+++ lib/extensions/forms/user_explore/slider_view.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
 
 class SliderView extends StatefulWidget {
-  const SliderView({super.key});
+  const SliderView(
+      {required this.onChagedlistValue, required this.listValue, super.key});
 
+  final Function(double) onChangedlistValue;
+  final double listValue;
   @override
   State<SliderView> createState() => _SliderViewState();
 }
 
 class _SliderViewState extends State<SliderView> {
+  double listValue = 50.0;
+
+  @override
+  void initState() {
+    super.initState();
+    listValue = widget.listValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: SizedBox(),
+              flex: listValue.round(),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
