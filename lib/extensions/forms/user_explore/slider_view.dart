ea2b05725d88dd6ffcdbbd3cfbba46a5bb131556
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-29 17:00:45 +0100

    Update slider_view.dart

--- lib/extensions/forms/user_explore/slider_view.dart
+++ lib/extensions/forms/user_explore/slider_view.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hooliday/extensions/constants.dart';
 
 class SliderView extends StatefulWidget {
   const SliderView(
-      {required this.onChagedlistValue, required this.listValue, super.key});
+      {required this.onChangedlistValue, required this.listValue, super.key});
 
   final Function(double) onChangedlistValue;
   final double listValue;
@@ -28,10 +29,53 @@ class _SliderViewState extends State<SliderView> {
             Expanded(
               child: SizedBox(),
               flex: listValue.round(),
+            ),
+            SizedBox(
+              width: 170,
+              child: Text(
+                'Less than ${(listValue / 10).toStringAsFixed(1)} Km',
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Expanded(
+              child: SizedBox(),
+              flex: 100 - listValue.round(),
             )
           ],
-        )
+        ),
+        SliderTheme(
+            data: SliderThemeData(
+              thumbShape: CustomThumbShape(),
+            ),
+            child: Slider(
+              onChanged: (double value) {
+                if (mounted) {
+                  setState(() {
+                    listValue = value;
+                  });
+                }
+                try {
+                  widget.onChangedlistValue(listValue);
+                } catch (_) {}
+              },
+              max: 100,
+              activeColor: HotelAppTheme.buildLightTheme().primaryColor,
+              inactiveColor: Colors.grey.withOpacity(0.4),
+              value: listValue,
+            )),
       ],
     );
+  }
+}
+
+class CustomThumbShape extends SliderComponentShape {
+  static const double _thumbSize = 3.0;
+  static const double _disabledThumSize = 3.0;
+
+  @override
+  Size getPreferredSize(bool isEnabled, bool isDiscrete) {
+    return isEnabled
+        ? const Size.fromRadius(_thumbSize)
+        : const Size.fromRadius(_disabledThumSize);
   }
 }
