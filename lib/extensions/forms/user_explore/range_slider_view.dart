817232cf73ee7e11e44bb5e4b235160fc8abb9a9
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-30 17:59:49 +0100

    Update range_slider_view.dart

--- lib/extensions/forms/user_explore/range_slider_view.dart
+++ lib/extensions/forms/user_explore/range_slider_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hooliday/extensions/constants.dart';
 
 class RangeSliderView extends StatefulWidget {
   const RangeSliderView(
@@ -20,6 +21,69 @@ class _RangeSliderViewState extends State<RangeSliderView> {
   }
 
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Stack(children: [
+          Row(
+            children: [
+              Expanded(
+                child: const SizedBox(),
+                flex: _values.start.round(),
+              ),
+              SizedBox(
+                width: 54,
+                child: Text(
+                  '\$${_values.start.round()}',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Expanded(
+                flex: 1000 - _values.start.round(),
+                child: const SizedBox(),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: _values.end.round(),
+                child: const SizedBox(),
+              ),
+              SizedBox(
+                width: 54,
+                child: Text(
+                  '\$${_values.end.round()}',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Expanded(
+                child: SizedBox(),
+                flex: 1000 - _values.end.round(),
+              )
+            ],
+          )
+        ]),
+        SliderTheme(
+            data: SliderThemeData(
+              rangeThumbShape: CustomRangeThumbShape(),
+            ),
+            child: RangeSlider(
+                values: _values,
+                max: 1000.0,
+                activeColor: HotelAppTheme.buildLightTheme().primaryColor,
+                inactiveColor: Colors.grey.withOpacity(0.4),
+                divisions: 1000,
+                onChanged: (RangeValues values) {
+                  try {
+                    if (mounted) {
+                      setState(() {
+                        _values = values;
+                      });
+                    }
+                    widget.onChangeRangeValues(_values);
+                  } catch (_) {}
+                }))
+      ],
+    );
   }
 }
