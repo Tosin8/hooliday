d289ff1d8ca0193f7e9fa83f3b8642386faa2b37
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-30 17:46:23 +0100

    Update range_slider_view.dart

--- lib/extensions/forms/user_explore/range_slider_view.dart
+++ lib/extensions/forms/user_explore/range_slider_view.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
 class RangeSliderView extends StatefulWidget {
-  const RangeSliderView({super.key});
+  const RangeSliderView(
+      {required this.values, required this.onChangeRangeValues, super.key});
 
+  final Function(RangeValues) onChangeRangeValues;
+  final RangeValues values;
   @override
   State<RangeSliderView> createState() => _RangeSliderViewState();
 }
 
 class _RangeSliderViewState extends State<RangeSliderView> {
+  late RangeValues _values;
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _values = widget.values;
+  }
+
   Widget build(BuildContext context) {
     return const Placeholder();
   }
