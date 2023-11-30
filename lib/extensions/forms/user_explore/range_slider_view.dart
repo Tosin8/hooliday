175b9f786ec0d18b493a4dfd78030b56af97a60e
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-30 18:24:14 +0100

    Update range_slider_view.dart

--- lib/extensions/forms/user_explore/range_slider_view.dart
+++ lib/extensions/forms/user_explore/range_slider_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooliday/extensions/constants.dart';
+import 'package:hooliday/extensions/forms/user_explore/slider_view.dart';
 
 class RangeSliderView extends StatefulWidget {
   const RangeSliderView(
@@ -86,4 +87,66 @@ class _RangeSliderViewState extends State<RangeSliderView> {
       ],
     );
   }
+}
+
+class CustomRangeThumbShape extends RangeSliderThumbShape {
+  static const double _thumbSize = 3.0;
+  static const double _disabledThumbSize = 3.0;
+
+  @override
+  Size getPreferredSize(bool isEnabled, bool isDiscrete) {
+    return isEnabled
+        ? const Size.fromRadius(_thumbSize)
+        : const Size.fromRadius(_disabledThumbSize);
+  }
+
+  static final Animatable<double> sizeTween =
+      Tween<double>(begin: _disabledThumbSize, end: _thumbSize);
+
+  @override
+  void paint(
+    PaintingContext context,
+    Offset center, {
+    required Animation<double> activationAnimation,
+    required Animation<double> enableAnimation,
+    required SliderThemeData sliderTheme,
+    bool isDiscrete = false,
+    bool isEnabled = false,
+    bool isOnTop = false,
+    bool isPressed = false,
+    TextDirection textDirection = TextDirection.ltr,
+    Thumb thumb = Thumb.start,
+  }) {
+    final Canvas canvas = context.canvas;
+    final ColorTween colorTween = ColorTween(
+        begin: sliderTheme.disabledThumbColor, end: sliderTheme.thumbColor);
+    final double size = _thumbSize * sizeTween.evaluate(enableAnimation);
+    Path thumbPath;
+    switch (textDirection) {
+      case TextDirection.ltr:
+        switch (thumb) {
+          case Thumb.start:
+            thumbPath = _rightTriangle(size, center);
+            break;
+
+          case Thumb.end:
+            thumbPath = _leftTriangle(size, center);
+            break;
+         }
+         break;
+         case TextDirection.ltr: 
+         switch (thumb) {
+          case Thumb.start: 
+          thumbPath = _leftTriange(size, center); 
+          break; 
+          case Thumb.end: thumbPath = _rightTriange(size, center); 
+          break; 
+    }
+    break; 
+  }
+
+  canvas.drawPath(Path()..addOval(Rect.fromPoints(Offset(center.dx + 12, center.dy * 12), 
+  Offset(center.dx - 12, center.dy - 12))) 
+  ..fillType = PathFillType.evenOdd, Paint()..color = Colors.black.withOpacity(0.5) ..maskFilter = MaskFilter.blur(BlurStyle.normal, convertRadiusToSigma(8)));
+
 }
