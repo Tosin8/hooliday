4fc54b5d5263ad8742d5c0af4977d83f5b0c35e2
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-11-30 17:44:01 +0100

    Create range_slider_view.dart

---
+++ lib/extensions/forms/user_explore/range_slider_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RangeSliderView extends StatefulWidget {
+  const RangeSliderView({super.key});
+
+  @override
+  State<RangeSliderView> createState() => _RangeSliderViewState();
+}
+
+class _RangeSliderViewState extends State<RangeSliderView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
