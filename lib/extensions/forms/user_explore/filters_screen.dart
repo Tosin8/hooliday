dfebe13dbff9cc17962a76839e54faa5c51e2caf
Tosin Ezekiel <23019300+Tosin8@users.noreply.github.com>
2023-12-02 18:37:52 +0100

    Update filters_screen.dart

--- lib/extensions/forms/user_explore/filters_screen.dart
+++ lib/extensions/forms/user_explore/filters_screen.dart
@@ -8,8 +8,82 @@ class FiltersScreen extends StatefulWidget {
 }
 
 class _FiltersScreenState extends State<FiltersScreen> {
+  List<PopularFilterListData> popularFilterListData =
+      PopularFilterListData.popularFList;
+  List<PopularFilterListData> accomodationListData =
+      PopularFilterListData.accomodationList;
+
+  RangeValues _values = const RangeValues(100, 600);
+  double distValue = 50.0;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Material(
+
+    color: HotelAppTheme.buildLightTheme().colorScheme.background,
+      child: Column(
+        children: <Widget>[
+          getAppBarUI(),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  priceBarFilter(),
+                  const Divider(
+                    height: 1,
+                  ),
+                  popularFilter(),
+                  const Divider(
+                    height: 1,
+                  ),
+                  distanceViewUI(),
+                  const Divider(
+                    height: 1,
+                  ),
+                  allAccommodationUI()
+                ],
+              ),
+            ),
+          ),
+          const Divider(
+            height: 1,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.only(left: 16, right: 16, bottom: 16, top: 8),
+            child: Container(
+              height: 48,
+              decoration: BoxDecoration(
+                color: HotelAppTheme.buildLightTheme().primaryColor,
+                borderRadius: const BorderRadius.all(Radius.circular(24.0)),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.6),
+                    blurRadius: 8,
+                    offset: const Offset(4, 4),
+                  ),
+                ],
+              ),
+              child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  borderRadius: const BorderRadius.all(Radius.circular(24.0)),
+                  highlightColor: Colors.transparent,
+                  onTap: () => Navigator.pop(context),
+                  child: const Center(
+                    child: Text(
+                      'Apply',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                          color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
-}
