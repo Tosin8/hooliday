3399308b9e6d529ba476aca736aa5d5e845db4e2
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-28 20:01:09 +0100

    Update splash_button.dart

--- lib/screens/onboarding/splash_button.dart
+++ lib/screens/onboarding/splash_button.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
 class OnboardButton extends StatelessWidget {
@@ -5,20 +6,24 @@ class OnboardButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector( 
-    onTap: () {},
-    child: Container(
-      height: 50,
-      width: 300,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(30)
+    return FadeInUp(
+      curve: Curves.bounceIn, 
+      
+      child: GestureDetector( 
+      onTap: () {},
+      child: Container(
+        height: 50,
+        width: 300,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30)
+        ),
+      child: const Align(
+      child: Text('Explore Now', 
+      style: TextStyle(color: Colors.black),),
       ),
-    child: const Align(
-    child: Text('Explore Now', 
-    style: TextStyle(color: Colors.black),),
-    ),
-    ),
+      ),
+      ),
     );
   }
 }
