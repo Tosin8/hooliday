c153a986522b9e1e2ed0df63ee732ce43b697940
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-05-28 20:42:07 +0100

    adding validators

--- lib/screens/forms/signin/login.dart
+++ lib/screens/forms/signin/login.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
@@ -17,110 +18,139 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  
+  final _emailController = TextEditingController();
+  final _pwdController = TextEditingController(); 
+
+  @override
+  void dispose() {
+    _pwdController.dispose();
+    _emailController.dispose();
+    
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start, 
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 100,), 
-              const Text('Hey, \nWelcome Back', 
-              style: TextStyle(fontSize: 20, 
-              fontWeight: FontWeight.w600),
-              ), 
-              const SizedBox(height: 10,),
-              const Text('Kindly fill in your details below', style: TextStyle(
-                color: Colors.grey, 
-              fontSize: 15),), 
-              const SizedBox(height: 20,), 
-
-
-              Form(
-                child: Column(
-                  children: [
-                    const SizedBox(height: 30,), 
-                    // Email ID 
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      textInputAction: TextInputAction.next,
-                      decoration:  const InputDecoration(
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-prefixIcon: Icon(Iconsax.message), 
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(12.0)), 
-                      ),
-
-                      hintText: 'Email ID',
-                      hintStyle: TextStyle(color: Colors.grey)
-                      ),
-                    ), 
-const SizedBox(height: 20,), 
-
-
-                    // Password 
-                    TextFormField(
-                      obscureText: true,
-                      keyboardType: TextInputType.visiblePassword,
-                      textInputAction: TextInputAction.done,
-                      decoration:  const InputDecoration(
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-prefixIcon: Icon(Iconsax.lock), 
-suffixIcon: Icon(Iconsax.eye_slash),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(12.0)),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(12.0)), 
-                      ),
-
-                      hintText: 'Password',
-                      hintStyle: TextStyle(color: Colors.grey)
-                      ),
-                    ), 
-                
- const SizedBox(height: 10,), 
-                   
-                      TextButton(onPressed: (){}, 
-                      child: const Text('Forgot Password?', 
+        child: FadeInLeft( 
+          duration: const Duration(milliseconds: 1000),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start, 
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 100,), 
+                const Text('Hola, \nWelcome Back', 
+                style: TextStyle(fontSize: 20, 
+                fontWeight: FontWeight.w600),
+                ), 
+                const SizedBox(height: 10,),
+                const Text('Kindly fill in your details below', style: TextStyle(
+                  color: Colors.grey, 
+                fontSize: 15),), 
+                const SizedBox(height: 20,), 
+          
+          
+                Form(
+                  key: _formKey, 
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 30,), 
+                      // Email ID 
+                      TextFormField(
+                        style: TextStyle(color: Colors.white),
+                        controller: _emailController, 
+                        validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please fill this field';
+                    }
+                    return null;
+                  },
+                        keyboardType: TextInputType.emailAddress,
+                        textInputAction: TextInputAction.next,
+                        decoration:  const InputDecoration(
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+          prefixIcon: Icon(Iconsax.message, color: Colors.white,), 
+          filled: true,
+          fillColor: Colors.black,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)), 
+                        ),
+          
+                        hintText: 'Email ID',
+                        hintStyle: TextStyle(color: Colors.white)
+                        ),
+                      ), 
+          const SizedBox(height: 20,), 
+          
+          
+                      // Password 
+                      TextFormField(
+controller: _pwdController,
+                        obscureText: true,
+                        //obscuringCharacter: '',
+                        keyboardType: TextInputType.visiblePassword,
+                        textInputAction: TextInputAction.done,
+                        decoration:  const InputDecoration(
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+          prefixIcon: Icon(Iconsax.lock), 
+          suffixIcon: Icon(Iconsax.eye_slash),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12.0)), 
+                        ),
+          
+                        hintText: 'Password',
+                        hintStyle: TextStyle(color: Colors.grey)
+                        ),
+                      ), 
+                  
+           const SizedBox(height: 10,), 
+                     
+                        TextButton(onPressed: (){}, 
+                        child: const Text('Forgot Password?', 
+                        
+                        style: TextStyle(color: Colors.black),
+                        
+                        )),
                       
-                      style: TextStyle(color: Colors.black),
+                      // Button. 
+                          const SizedBox(height: 30,), 
+                      const SignInButton(), 
+                      const SizedBox(height: 20,), 
+                      const Text('Or Continue With', 
+                      style: TextStyle(color: Colors.grey),), 
+                      const SizedBox(height: 20,), 
+          
+                      // social media button.
+                      const GoogleButton(), 
+                      const SizedBox(height: 10,), 
+                       Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+          
+                const Text('Don\'t have an account?', style: TextStyle(color: Colors.grey),),
+          
+                TextButton(onPressed: () {
+          Get.to(() => const SignUp());
+                },  child: const Text('Sign Up', style: TextStyle(color: Colors.black),))
+              ],
+            )
                       
-                      )),
-                    
-                    // Button. 
-                        const SizedBox(height: 30,), 
-                    const SignInButton(), 
-                    const SizedBox(height: 20,), 
-                    const Text('Or Continue With', 
-                    style: TextStyle(color: Colors.grey),), 
-                    const SizedBox(height: 20,), 
-
-                    // social media button.
-                    const GoogleButton(), 
-                    const SizedBox(height: 10,), 
-                     Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-
-      const Text('Don\'t have an account?', style: TextStyle(color: Colors.grey),),
-
-      TextButton(onPressed: () {
-        Get.to(() => const SignUp());
-      },  child: const Text('Sign Up', style: TextStyle(color: Colors.black),))
-    ],
-  )
-                    
-                  ],
-                ),
-              ), 
-            ],
+                    ],
+                  ),
+                ), 
+              ],
+            ),
           ),
         ),
       )
--- lib/screens/forms/signin/widgets/buttons/login_button.dart
+++ lib/screens/forms/signin/widgets/buttons/login_button.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
 
-class SignInButton extends StatelessWidget {
+class SignInButton extends StatefulWidget {
   const SignInButton({super.key});
 
   @override
+  State<SignInButton> createState() => _SignInButtonState();
+}
+
+class _SignInButtonState extends State<SignInButton> {
+   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  
+  final _emailController = TextEditingController();
+  final _pwdController = TextEditingController(); 
+
+  @override
+  void dispose() {
+    _pwdController.dispose();
+    _emailController.dispose();
+    
+    super.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
     return GestureDetector( 
-                      onTap: () {},
+                      onTap: () {
+                          if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(
+                                'Hello ${_emailController.text}\nYour details have been submitted and an email sent to ${_emailController.text}.')));
+                      } else {
+                        // The form has some validation errors.
+                        // Do Something...
+                      }
+                      },
                       child: Container(
                         height: 60, width: 300, 
                         decoration: BoxDecoration(
