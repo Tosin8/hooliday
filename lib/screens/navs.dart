4c61df8255cd32a27ada02fc7a26c12885419f9d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-14 00:55:42 +0100

    Update navs.dart

--- lib/screens/navs.dart
+++ lib/screens/navs.dart
@@ -1,78 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
-import 'package:hooliday/screens/favorite.dart';
-import 'package:hooliday/screens/home_screen.dart';
-import 'package:hooliday/screens/profile.dart';
-import 'package:hooliday/screens/search.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:multiva/screen/productList.dart';
 
+import 'screen/cart.dart';
+import 'screen/profile.dart';
 
-class NavsScreen extends StatefulWidget {
-  const NavsScreen({super.key});
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
 
   @override
-  State<NavsScreen> createState() => _NavsScreenState();
+  State<BottomNav> createState() => _BottomNavState();
 }
 
-class _NavsScreenState extends State<NavsScreen> {
+class _BottomNavState extends State<BottomNav> {
+  int index = 0;
+
+  final screens = [
+    ProductScreen(), 
+    CartScreen(),
+    ProfileScreen(),
+  ]; 
   @override
   Widget build(BuildContext context) {
-     int currentIndex = 0;
-  final List<Widget> _children = [
-    const HomeScreen(),
-    const UserProfile(),
-    const UserExplore(),
-    const UserFavs()
-  ];
-
-  void changePage(int index) {
-    setState(() {
-      currentIndex = index;
-    });
-  }
 
     return Scaffold(
-      body: _children[currentIndex],
-      bottomNavigationBar: BubbleBottomBar(
-        opacity: .2,
-        currentIndex: currentIndex,
-        onTap: (index) => changePage,
-        borderRadius: const BorderRadius.vertical(top: Radius.circular(16.0)),
-        elevation: 8,
-        fabLocation: BubbleBottomBarFabLocation.end, //new
-        hasNotch: true, //new
-        hasInk: true, //new, gives a cute ink effect
-        inkColor: Colors.black12, //optional, uses splashColor if not specified
-        items: const <BubbleBottomBarItem>[
-         BubbleBottomBarItem(
-          showBadge: true,
-          badge: Text("5"),
-
-            backgroundColor: Colors.red,
-            icon: Icon(Icons.home, color: Colors.black),
-            activeIcon: Icon(Icons.home, color: Colors.red),
-            title: Text("Home"),
-          ),
-           BubbleBottomBarItem(
-            backgroundColor: Colors.deepPurple,
-            icon: Icon(Icons.person, color: Colors.black),
-            activeIcon: Icon(Icons.person, color: Colors.deepPurple),
-            title: Text("Profile"), 
-          ),
-           BubbleBottomBarItem(
-            backgroundColor: Colors.indigo,
-            icon: Icon(Icons.search, color: Colors.black),
-            activeIcon: Icon(Icons.search, color: Colors.indigo),
-            title: Text("Search"),
-          ),
-           BubbleBottomBarItem(
-            backgroundColor: Colors.green,
-            icon: Icon(Icons.favorite, color: Colors.black),
-            activeIcon: Icon(Icons.favorite, color: Colors.green),
-            title: Text("Favorite"),
-          ),
-        ],
-      ),
+      body: screens[index],
+      bottomNavigationBar:
+      
+      NavigationBarTheme(
+        data: NavigationBarThemeData(
+          indicatorColor: Colors.pink, 
+          labelTextStyle: WidgetStateProperty.all(TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.white))), 
+       child:
+       NavigationBar(
+        height: 80, 
+        backgroundColor: Colors.black,
+        selectedIndex: index,
+        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+        animationDuration: const Duration(seconds: 1),
+        onDestinationSelected: (int index) => setState(() => this.index = index),
+        destinations: [
+          NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+          NavigationDestination(icon: Icon(Iconsax.shopping_cart), label: 'Cart'),
+          NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+        ],),), 
     );
   }
 }
 
+
+// home, profile, search , favorites. 
