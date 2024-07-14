d1251fb84ad6bd1800eb584006c5678849529820
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-14 03:48:48 +0100

    Recreating login form

--- lib/screens/forms/signin/login.dart
+++ lib/screens/forms/signin/login.dart
@@ -29,190 +29,197 @@ class _SignInState extends State<SignIn> {
         duration: const Duration(milliseconds: 1000),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 100,
-              ),
-              const Text(
-                'Hola, \nWelcome Back',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                'Kindly fill in your details below',
-                style: TextStyle(color: Colors.grey, fontSize: 15),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    // Email ID
-                    TextFormField(
-                      style: const TextStyle(color: Colors.white),
-                      controller: _controller.emailController,
-                      validator: _controller.validateEmail,
-                      keyboardType: TextInputType.emailAddress,
-                      textInputAction: TextInputAction.next,
-                      decoration: const InputDecoration(
-                          floatingLabelBehavior: FloatingLabelBehavior.always,
-                          prefixIcon: Icon(
-                            Iconsax.message,
-                            color: Colors.white,
-                          ),
-                          filled: true,
-                          fillColor: Colors.black,
-                          border: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(12.0)),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(12.0)),
-                          ),
-                          hintText: 'Email ID',
-                          hintStyle: TextStyle(color: Colors.grey)),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-
-                    // Password
-                    Obx(() => TextFormField(
-                          style: const TextStyle(color: Colors.white),
-
-                          controller: _controller.passwordController,
-                          obscureText: _controller.isPasswordHidden.value,
-                          validator: _controller.validatePassword,
-                          //obscuringCharacter: '',
-                          keyboardType: TextInputType.visiblePassword,
-                          textInputAction: TextInputAction.done,
-                          decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Colors.black,
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.always,
-                              prefixIcon: const Icon(
-                                Iconsax.lock,
-                                color: Colors.white,
-                              ),
-                              suffixIcon: IconButton(
-                                onPressed: _controller.togglePasswordVisibility,             icon: Icon(_controller.isPasswordHidden.value ?
-                                  Iconsax.eye_slash: Iconsax.eye,
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                
+              )
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 100,
+                ),
+                const Text(
+                  'Hola, \nWelcome Back',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'Kindly fill in your details below',
+                  style: TextStyle(color: Colors.grey, fontSize: 15),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      // Email ID
+                      TextFormField(
+                        style: const TextStyle(color: Colors.white),
+                        controller: _controller.emailController,
+                        validator: _controller.validateEmail,
+                        keyboardType: TextInputType.emailAddress,
+                        textInputAction: TextInputAction.next,
+                        decoration: const InputDecoration(
+                            floatingLabelBehavior: FloatingLabelBehavior.always,
+                            prefixIcon: Icon(
+                              Iconsax.message,
+                              color: Colors.white,
+                            ),
+                            filled: true,
+                            fillColor: Colors.black,
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(12.0)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(12.0)),
+                            ),
+                            hintText: 'Email ID',
+                            hintStyle: TextStyle(color: Colors.grey)),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+            
+                      // Password
+                      Obx(() => TextFormField(
+                            style: const TextStyle(color: Colors.white),
+            
+                            controller: _controller.passwordController,
+                            obscureText: _controller.isPasswordHidden.value,
+                            validator: _controller.validatePassword,
+                            //obscuringCharacter: '',
+                            keyboardType: TextInputType.visiblePassword,
+                            textInputAction: TextInputAction.done,
+                            decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.black,
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.always,
+                                prefixIcon: const Icon(
+                                  Iconsax.lock,
                                   color: Colors.white,
                                 ),
-                              ),
-                              border: const OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(12.0)),
-                              ),
-                              focusedBorder: const OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(12.0)),
-                              ),
-                              hintText: 'Password',
-                              hintStyle: const TextStyle(color: Colors.grey)),
-                        )),
-
-                    const SizedBox(
-                      height: 10,
-                    ),
-
-                    TextButton(
-                        onPressed: () {},
-                        child: const Text(
-                          'Forgot Password?',
-                          style: TextStyle(color: Colors.black),
-                        )),
-
-                    // Button.
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    GestureDetector(
-                      onTap: _controller.isLoading.value ? null :  () => _controller.submitForm(_formKey),
-                      child: Container(
-                        height: 60,
-                        width: 300,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30.0),
-                            color: Colors.black),
-                        child: _controller.isLoading.value
-                            ? const Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  SizedBox(
-                                    height: 20,
-                                    width: 20,
-                                    child: CircularProgressIndicator(
-                                        color: Colors.white, strokeWidth: 2.0),
+                                suffixIcon: IconButton(
+                                  onPressed: _controller.togglePasswordVisibility,             icon: Icon(_controller.isPasswordHidden.value ?
+                                    Iconsax.eye_slash: Iconsax.eye,
+                                    color: Colors.white,
                                   ),
-                                  SizedBox(
-                                    width: 10,
-                                  ),
-                                  Text(
-                                    'Please Wait',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                ],
-                              )
-                            : const Align(
-                                child: Text(
-                                  'Login',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 16),
                                 ),
-                              ),
+                                border: const OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(12.0)),
+                                ),
+                                focusedBorder: const OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(12.0)),
+                                ),
+                                hintText: 'Password',
+                                hintStyle: const TextStyle(color: Colors.grey)),
+                          )),
+            
+                      const SizedBox(
+                        height: 10,
                       ),
-                    ),
-
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const Text(
-                      'Or Continue With',
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-
-                    // social media button.
-                    const GoogleButton(),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Don\'t have an account?',
-                          style: TextStyle(color: Colors.grey),
+            
+                      TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Forgot Password?',
+                            style: TextStyle(color: Colors.black),
+                          )),
+            
+                      // Button.
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      GestureDetector(
+                        onTap: _controller.isLoading.value ? null :  () => _controller.submitForm(_formKey),
+                        child: Container(
+                          height: 60,
+                          width: 300,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30.0),
+                              color: Colors.black),
+                          child: _controller.isLoading.value
+                              ? const Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SizedBox(
+                                      height: 20,
+                                      width: 20,
+                                      child: CircularProgressIndicator(
+                                          color: Colors.white, strokeWidth: 2.0),
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      'Please Wait',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ],
+                                )
+                              : const Align(
+                                  child: Text(
+                                    'Login',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 16),
+                                  ),
+                                ),
                         ),
-                        TextButton(
-                            onPressed: () {
-                              Get.to(() => const SignUp());
-                            },
-                            child: const Text(
-                              'Sign Up',
-                              style: TextStyle(color: Colors.black),
-                            ))
-                      ],
-                    )
-                  ],
+                      ),
+            
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text(
+                        'Or Continue With',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+            
+                      // social media button.
+                      const GoogleButton(),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Don\'t have an account?',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                Get.to(() => const SignUp());
+                              },
+                              child: const Text(
+                                'Sign Up',
+                                style: TextStyle(color: Colors.black),
+                              ))
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
